f3416b820de34a3b6bb916141e6ec180efd6b0f0
Maxime Devillet <maxime.devillet01@gmail.com>
2023-03-14 21:20:49 +0100

    rr

--- Assignement2/softflow.py
+++ Assignement2/softflow.py
@@ -10,14 +10,18 @@ class SoftFlow(Problem):
     def __init__(self, initial):
         super().__init__(initial)
         self.state_intit = initial
+        self.state_intit.posexam()
+        self.liste = []
         
     def actions(self, state):
         actions = []
-        for pos, exam in zip(state.pos.values(), state.exam.values()):
-            x, y = pos
+        new_pos = state.pos.copy()
+        for pos, exam in zip(state.pos, state.exam.values()):
+            x, y = state.pos[pos]
             listes_poss = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
             for poss in listes_poss:
-                if state.grid[poss[0]][poss[1]] == " " and self.distance2(pos,exam) == False:
+                new_pos[pos] = (x,y)
+                if state.grid[poss[0]][poss[1]] == " " and self.liste.count(state.grid)==0:
                     actions.append((state.grid[x][y],poss))
         return actions
 
@@ -27,22 +31,33 @@ class SoftFlow(Problem):
             new_grid.append(i[:])
 
         new_pos = state.pos.copy()
+        new_exam = state.exam.copy()
 
-        new_state = State(new_grid, new_pos, state.exam)
+        new_state = State(new_grid, new_pos, new_exam)
         value = action[0]
-        new_state.grid[state.pos[value][0]][state.pos[value][1]] = str(value)
+        new_state.grid[state.pos[value][0]][state.pos[value][1]] = str(ord(value)-97)
         if (state.grid[action[1][0]][action[1][1]].isdigit() == False):
             new_state.grid[action[1][0]][action[1][1]] = action[0]
         new_pos[value] = action[1]
-        char = str(ord(action[0])-97)
-        if(self.distance2(new_pos[action[0]],state.exam[char])):
-            del new_pos[action[0]]
 
-        print(new_state)
+        nn = 
+
+        self.liste.append(new_pos)
+        print(state)
         return new_state
 
     def goal_test(self, state):
-        return state.pos == state.exam
+        h = 0
+        for pos,exam in zip(state.pos.values(), state.exam.values()):
+            
+            h+= abs(pos[0] - exam[0])
+            h+= abs(pos[1] - exam[1])
+        if(h <=len(state.pos)):
+            for i in range (len(state.grid)):
+                for j in range (len(state.grid[i])):
+                    if state.grid[i][j].isalpha():
+                        state.grid[i][j] = str(ord(state.grid[i][j])-97)
+        return h <=len(state.pos)
 
     def h(self, node):
         state = node.state
@@ -52,7 +67,16 @@ class SoftFlow(Problem):
             h+= abs(pos[0] - exam[0])
             h+= abs(pos[1] - exam[1])
 
-        return h
+        return h+(100/self.nbr(state))
+
+    
+    def nbr(self,state):
+        count = 1
+
+        for key in state.pos:
+            if self.distance2(state.pos[key],state.exam[str(ord(key)-97)]):
+                count += 1
+        return count
     
     def distance2(self,pos,exam):
         h = 0
@@ -68,6 +92,7 @@ class SoftFlow(Problem):
             lines = f.readlines()
             
         state = State.from_string(''.join(lines))
+        state.posexam()
         return SoftFlow(state)
 
 
@@ -78,13 +103,21 @@ class SoftFlow(Problem):
 
 class State:
 
-    def __init__(self, grid, pos=None, exam=None):
+    def __init__(self, grid,pos={},exam={}):
         self.nbr = len(grid)
         self.nbc = len(grid[0])
         self.grid = grid
         self.pos = pos
         self.exam = exam
-        
+
+    def posexam(self):
+        for i, line in enumerate(self.grid):
+            for j, x in enumerate(line):
+                if(x != "#" and x!= " "):
+                    if x.isalpha():
+                        self.pos[x] = (i, j)
+                    else:
+                        self.exam[x] = (i, j)
     def __str__(self):
         return '\n'.join(''.join(row) for row in self.grid)
 
@@ -100,16 +133,8 @@ class State:
     def from_string(string):
         lines = string.strip().splitlines()
         grid = list(map(lambda x: list(x.strip()), lines))
-        pos = {}
-        exam = {}
-        for i, line in enumerate(lines):
-            for j, x in enumerate(line.strip()):
-                if(x != "#" and x!= " "):
-                    if x.isalpha():
-                        pos[x] = (i, j)
-                    else:
-                        exam[x] = (i, j)
-        return State(grid, pos, exam)
+
+        return State(grid)
 
 
 
@@ -121,10 +146,6 @@ class State:
 
 problem = SoftFlow.load(sys.argv[1])
 
-#print(problem.state_intit.pos)
-#print(problem.state_intit.exam)
-#a = problem.actions(problem.state_intit)
-#print(a)
 
 
 # example of print
@@ -134,8 +155,8 @@ def test():
 
     print('Number of moves: ', str(node.depth))
     print(node.state)
-    #for n in path:
-        #print(n.state)  # assuming that the _str_ function of state outputs the correct format
-        #print()
+    for n in path:
+        print(n.state)  # assuming that the _str_ function of state outputs the correct format
+        print()
 
 test()
